1e137edbb84c02259e78bc71a649c66d5567733e
Lingge Zhao <zhalingg@shernet.sheridancollege.ca>
2024-09-29 18:01:11 -0400

    1A done.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -8,10 +8,34 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
+    public float moveSpeed = 5f;
 
     void Update()
     {
-
+        PlayerMovement();
     }
 
+    void PlayerMovement()
+    {
+        Vector3 velocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity += Vector3.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocity += Vector3.down;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity += Vector3.right;
+        }
+
+        transform.position += moveSpeed * Time.deltaTime * velocity;
+    }
 }
