1f909821d0f3854f9e1f2f98d27c09197f47aea6
Lingge Zhao <zhalingg@shernet.sheridancollege.ca>
2024-10-13 18:58:27 -0400

    Week4-Task2 done.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -14,11 +14,18 @@ public class Player : MonoBehaviour
     private float currentSpeed = 0f;
     public float radius = 3f;
     public int circlePoints = 30;
+    public GameObject powerupPrefab;
+    public int numberOfPowerups = 5;
+    public float spawnRadius = 5f;
 
     void Update()
     {
         PlayerMovement();
         EnemyRadar(radius, circlePoints);
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            SpawnPowerups(spawnRadius, numberOfPowerups);
+        }
     }
 
     void PlayerMovement()
@@ -85,6 +92,21 @@ public class Player : MonoBehaviour
             Vector3 nextPoint = new Vector3(Mathf.Cos(nextAngleRad) * radius, Mathf.Sin(nextAngleRad) * radius, 0) + playerPosition;
 
             Debug.DrawLine(nowPoint, nextPoint, radarColor); 
+        }
+    }
+
+    public void SpawnPowerups(float radius, int numberOfPowerups)
+    {
+        float angleBetween = 360f / numberOfPowerups;
+
+        for (int i = 0; i < numberOfPowerups; i++)
+        {
+            float angleNow = i * angleBetween;
+            float angleRad = angleNow * Mathf.Deg2Rad;
+
+            Vector3 powerupPosition = new Vector3(Mathf.Cos(angleRad) * spawnRadius, Mathf.Sin(angleRad) * spawnRadius, 0) + transform.position;
+
+            Instantiate(powerupPrefab, powerupPosition, Quaternion.identity);
         }
     }
 }
