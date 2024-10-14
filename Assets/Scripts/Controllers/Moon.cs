4a2d027af3ecf60e52bb8928c3e98debc3769906
Lingge Zhao <zhalingg@shernet.sheridancollege.ca>
2024-10-13 20:56:02 -0400

    Week4-Task3 done.

--- Assets/Scripts/Controllers/Moon.cs
+++ Assets/Scripts/Controllers/Moon.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float orbitalSpeed = 10f;
+    public float radius = 3f;
+    private float nowAngle = 0f;
 
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(radius, orbitalSpeed, planetTransform);
+    }
+
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        nowAngle += speed * Time.deltaTime; 
+        Vector3 newPosition = target.position + new Vector3(Mathf.Cos(nowAngle), Mathf.Sin(nowAngle), 0) * radius;
+        transform.position = newPosition;
+
+        Vector3 toPlanet = target.position - transform.position;
+        float angleToPlanet = Mathf.Atan2(toPlanet.y, toPlanet.x) * Mathf.Rad2Deg;
 
     }
 }
